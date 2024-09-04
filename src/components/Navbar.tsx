d4a32cc574432da6f97a94f773e0022e7b5058ed
Nadav Peled <nadav.peled@post.runi.ac.il>
2024-09-04 16:44:06 +0300

    added some styling to the navigation bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/Navbar.css"; // Import custom CSS
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="navbar">
-      <ul className="nav-list">
-        <li className="nav-item">
-          <Link to="/how-to-use">How to Use the Website</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/login">Login</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/offer-ride">Offer a New Ride</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/take-ride">Take a Ride</Link>
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+      <div className="container-fluid">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/how-to-use">
+              How to Use the Website
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">
+              Login
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/offer-ride">
+              Offer a New Ride
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/take-ride">
+              Take a Ride
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
